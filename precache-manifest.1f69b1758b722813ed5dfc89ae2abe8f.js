474380824252ef1f3f155aa20d76216ba1536bdc
Dipesh Trikam <dipesht@datacom.co.nz>
2019-09-04 11:20:47 +1200

    Updates

---
+++ precache-manifest.1f69b1758b722813ed5dfc89ae2abe8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2425fdfc472ea3fa1b015e42e0961a",
+    "url": "/dtrikam-website/index.html"
+  },
+  {
+    "revision": "d2d0e4aba5c2d4a45115",
+    "url": "/dtrikam-website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f2552e8b2b1fe55ad515",
+    "url": "/dtrikam-website/static/js/2.bb888b93.chunk.js"
+  },
+  {
+    "revision": "d2d0e4aba5c2d4a45115",
+    "url": "/dtrikam-website/static/js/main.608bce94.chunk.js"
+  },
+  {
+    "revision": "472faebf2c56422e18c2",
+    "url": "/dtrikam-website/static/js/runtime~main.dd7409c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dtrikam-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
